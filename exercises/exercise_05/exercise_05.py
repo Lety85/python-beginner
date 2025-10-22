504eeae2448153df996a8e381bc74de9c24ddf50
Leticia <160972155+Lety85@users.noreply.github.com>
2025-10-22 16:38:03 +0200

    Refactor temperature conversion logic in Exercise 5 for clarity

--- exercises/exercise_05/exercise_05.py
+++ exercises/exercise_05/exercise_05.py
@@ -8,8 +8,16 @@
 
 # Get temperature in Celsius from user
 
+celsius = float(input("Enter the temperature in Celsius: "))
+
 # Convert to Fahrenheit
+
+fahrenheit = (celsius * 9/5) + 32
 
 # Convert to Kelvin
 
+kelvin = celsius + 273.15
+
 # Display the results
+
+print(f'{celsius}°C is equal to: \n{fahrenheit:.2f}°F (Fahrenheit) \n{kelvin:.2f} K (Kelvin)')

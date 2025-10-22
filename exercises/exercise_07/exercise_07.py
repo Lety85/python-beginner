7611f0b2cee14c52116e7985124f390430265d06
Leticia <160972155+Lety85@users.noreply.github.com>
2025-10-22 17:09:18 +0200

    Implement grade calculation logic in Exercise 7 to convert numerical grades to letter grades

--- exercises/exercise_07/exercise_07.py
+++ exercises/exercise_07/exercise_07.py
@@ -7,6 +7,21 @@
 
 # Get grade from user
 
+numerical = int(input("Enter your grade: "))
+
 # Determine letter grade using if/elif/else
+
+if numerical >= 90 and numerical <= 100:
+    print(f'Your letter grade is A')
+elif numerical >= 80 and numerical < 90:
+    print(f'Your letter grade is B')
+elif numerical >= 70 and numerical < 80:
+    print(f'Your letter grade is C')
+elif numerical >= 60 and numerical <70:
+    print(f'Your letter grade is D')
+elif numerical >= 0 and numerical <60:
+    print(f'Your grade is F')
+else:
+    print(f'Invalid number')
 
 # Print the letter grade

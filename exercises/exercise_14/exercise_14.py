23fe0c539ee6d0b91e32bde434cd7f819a200d0a
Leticia <160972155+Lety85@users.noreply.github.com>
2025-10-23 17:54:40 +0200

    Refactor Exercise 14 to enhance student grade book functionality with improved output and user interaction

--- exercises/exercise_14/exercise_14.py
+++ exercises/exercise_14/exercise_14.py
@@ -9,14 +9,30 @@
 
 # Create grade book with at least 3 students
 
+grades = {"Alice": 85, "Bob": 92, "Charlie": 78}
 
 # Display all students and grades
 
+print(f'Grade Book: ')
+for name, grade in grades.items():
+    print(f'{name} : {grade}')
 
 # Calculate and display average grade
 
+average = sum(grades.values())/ len(grades.values())
+print(f'Average grade: {average}')
 
 # Find and display top student
 
+top_student = max(grades, key = grades.get)
+top_grade = grades[top_student]
+print(f'Top student: {top_student} with grade {top_grade}')
 
 # Look up a specific student's grade
+
+students_name = input("Enter student name to look up: ")
+
+if students_name in grades:
+    print(f"{students_name}'s grade: {grades[students_name]}")
+else:
+    print("The student is not in the diccionary")

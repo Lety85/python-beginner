96ac6de2921383a984692df4e4cc790eef151336
Leticia <160972155+Lety85@users.noreply.github.com>
2025-10-23 14:06:46 +0200

    Refactor Exercise 11 to improve list operations and enhance output clarity

--- exercises/exercise_11/exercise_11.py
+++ exercises/exercise_11/exercise_11.py
@@ -7,17 +7,28 @@
 
 # Create a list of 5 favorite fruits
 
+fruits = ["strawberry", "melon", "banana", "grape", "apple"]
 
 # Display original list
 
+print(f'Original list of fruits: {fruits}')
 
 # Add two more fruits
 
+fruits.append("watermelon")
+fruits.append("lemon")
+print(f'List of fruits after adding: {fruits}')
 
 # Remove a fruit
 
+fruits.remove("apple")
+print(f'List of fruits after removing: {fruits}')
 
 # Sort the list
 
+fruits.sort()
+print(f'Sorted list of fruits: {fruits}')
 
 # Display final list and length
+
+print(f'Total fruits {len(fruits)}')

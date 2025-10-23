9cbe1632ad68da28cd65f211f2115334b1c67018
Leticia <160972155+Lety85@users.noreply.github.com>
2025-10-23 16:38:57 +0200

    Enhance string manipulation exercise by adding detailed output for user input analysis

--- exercises/exercise_13/exercise_13.py
+++ exercises/exercise_13/exercise_13.py
@@ -8,20 +8,34 @@
 
 # Get sentence from user
 
+sentence = input("Enter a sentence: ")
 
 # Display length
 
+print(f'Length: {len(sentence)}')
 
 # Display uppercase and lowercase
 
+print(f'Uppercase: {sentence.upper()}')
+print(f'Lowecase: {sentence.lower()}')
 
 # Display word count (use .split())
 
+print(f'Number of words: {len(sentence.split())}')
 
 # Display reversed string
 
+print(f'Reversed: {sentence[::-1]}')
 
 # Display first 3 characters
 
+print(f'First 3 characters: {sentence[:3]}')
 
 # Check if contains 'python' (case-insensitive)
+
+print(f'Contains Python: {"python" in sentence.lower()} ')
+
+if "python" in sentence.lower():
+    print(f'Contains Python: Yes')
+else:
+    print(f'Contains Python: No')

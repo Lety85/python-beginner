12ce5d862d5bb806a4f8b7822e9c2f05c7086354
Leticia <160972155+Lety85@users.noreply.github.com>
2025-10-27 17:35:17 +0100

    Refactor Exercise 12 to improve shopping list management with enhanced user interaction and clearer output

--- exercises/exercise_12/exercise_12.py
+++ exercises/exercise_12/exercise_12.py
@@ -8,8 +8,56 @@
 
 # Create empty shopping list
 
+shooping_list = []
 
 # Main program loop
+
+while True:
+    print(f'Shopping List Manager\n')
+    print("1. Add item")
+    print("2. View list")
+    print("3. Remove item")
+    print("4. Quit")
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        item = input("Enter item to add: ")
+        shooping_list.append(item)
+        print(f'{item} added to the list')
+
+    elif choice == "2":
+        if len(shooping_list) == 0:
+            print("Your shopping list is empty")
+        else:
+            print(f'Your shopping list:\n')
+            for index, item in enumerate(shooping_list,1):
+                print(f'{index}. {item}')
+
+    elif choice == "3":
+        if len(shooping_list) == 0:
+            print("Your shopping list is empty")
+        else:
+            print(f'Your shopping list:\n')
+            for index, item in enumerate(shooping_list,1):
+                print(f'{index}. {item}')
+
+            item_rem = int(input("Enter item number to remove: "))
+            if 1 <= item_rem <= len(shooping_list):
+                removed_item = shooping_list.pop(item_rem - 1)
+                print(f'{removed_item} removed from the list')
+            else:
+                print("Invalid item")
+
+    elif choice == "4":
+        print("Goodbye!")     
+        break 
+
+    else:
+        print("Invalid choice. Please choose 1-4")
+
+    
+
 
     # Display menu
 
